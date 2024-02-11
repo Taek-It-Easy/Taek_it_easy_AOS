fbe0c395b80336817bccb5fe188c6ac9d0009432
NaZe0320 <rwt1226@naver.com>
2024-02-12 02:59:06 +0900

    [add] 시리얼 넘버 구현 완료

--- lib/viewModel/user_provider.dart
+++ lib/viewModel/user_provider.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:taek_it_easy/constants.dart';
 import 'package:taek_it_easy/data/model/user.dart';
 import 'package:http/http.dart' as http;
@@ -14,5 +17,28 @@ class UserProvider with ChangeNotifier {
     _user = User(deviceNum: "12300", userAge: 12);
     final response = await http.post(endPoint,
         headers: Constants.headers, body: jsonEncode(user));
+
+    getModileId();
+  }
+
+  Future<String> getModileId() async {
+    final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+    String id = '';
+    try {
+      if (Platform.isAndroid) {
+        final AndroidDeviceInfo androidDeviceInfo =
+            await deviceInfoPlugin.androidInfo;
+
+        id = androidDeviceInfo.id;
+      } else if (Platform.isIOS) {
+        final IosDeviceInfo iosDeviceInfo = await deviceInfoPlugin.iosInfo;
+        id = iosDeviceInfo.identifierForVendor ?? '';
+      }
+    } on PlatformException {
+      id = '';
+    }
+    print('id: $id');
+    return id;
   }
 }
